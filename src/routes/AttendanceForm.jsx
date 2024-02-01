a794c3187e6864edff4c91f4fbca6e415ed5b9ed
imranilyas <68575807+imranilyas@users.noreply.github.com>
2024-02-01 01:09:36 -0800

    logout button

--- src/routes/AttendanceForm.jsx
+++ src/routes/AttendanceForm.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ListComponent from "../components/ListComponent";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deauthenticate } from "../redux/loginSlice";
 
 const AttendanceForm = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const logoutHandler = (e) => {
+		e.preventDefault();
+		dispatch(deauthenticate());
+		navigate("/");
+	};
+
 	return (
 		<div>
+			<button onClick={logoutHandler}>Log Out</button>
 			<ListComponent />
 		</div>
 	);
