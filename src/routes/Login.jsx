135a32b5d9b8f08d052ce8cf221840abbf50f628
imranilyas <68575807+imranilyas@users.noreply.github.com>
2024-02-01 18:08:50 -0800

    styling login form, background color

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { authenticate } from "../redux/loginSlice";
 import { useNavigate } from "react-router-dom";
 
+import "../style/Login.css";
+
 const Login = () => {
 	const authenticated = useSelector((state) => state.login.authorized);
 	const dispatch = useDispatch();
@@ -23,20 +25,22 @@ const Login = () => {
 	};
 
 	return (
-		<form onSubmit={submitHandler}>
+		<form onSubmit={submitHandler} className="loginForm">
 			<input
 				placeholder="Username"
 				type="text"
 				value={username}
 				onChange={(e) => setUsername(e.target.value)}
+				className="loginInput"
 			/>
 			<input
 				placeholder="Password"
 				type="password"
 				value={password}
 				onChange={(e) => setPassword(e.target.value)}
+				className="loginInput"
 			/>
-			<input type="submit" value={"Login"} />
+			<input type="submit" value={"Login"} className="loginInput" />
 		</form>
 	);
 };
