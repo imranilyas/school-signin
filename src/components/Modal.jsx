5976d58be7422d99fac8ed0937b7fb8629b07f63
imranilyas <68575807+imranilyas@users.noreply.github.com>
2024-02-19 20:27:35 -0800

    modal + styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import "../style/Modal.css";
 
 const Modal = ({ closeModal }) => {
 	return (
-		<div className="modalContainer">
-			<p>Are you sure you would like to submit the students</p>
-			<div className="modalButtonContainer">
-				<button onClick={closeModal}>Nope</button>
-				<button onClick={closeModal}>Confirm</button>
+		<div className="modalBackground">
+			<div className="modalContainer">
+				<div className="xBtn">
+					<button onClick={closeModal}>X</button>
+				</div>
+				<div className="modalTitle">
+					<h1>Are you sure you would like to continue?</h1>
+				</div>
+
+				<div className="modalBody">
+					<p>
+						Once submitted, the checked students will be confirmed
+						as having attended school today
+					</p>
+				</div>
+
+				<div className="modalButtonContainer">
+					<button id="cancelBtn" onClick={closeModal}>
+						Nope
+					</button>
+					<button onClick={closeModal}>Confirm</button>
+				</div>
 			</div>
 		</div>
 	);
